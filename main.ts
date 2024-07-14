8d3997ece3279724421d830a02521f1faee92002
Syeda-Rohab <syedarohab8@gmail.com>
2024-07-14 08:21:27 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,60 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+let myBalance = 10000; // Dollar
+let myPin = 1512;
+let pinAnswer = await inquirer.prompt([
+    
+    {
+    
+        name: "pin",
+        message: "Enter your pin",
+        type: "number",
+    }
+
+]);
+
+if (pinAnswer.pin === myPin) {
+    console.log("Correct pin code!!!");
+}
+
+let operationAns = await inquirer.prompt([
+    
+        {
+            name: "operation",
+            message: "Please select option",
+            type: "list",
+            choices: ["withdraw", "check balance"]
+        }
+    
+]);  
+
+if (operationAns.operation === "withdraw") {}
+    let amountAns = await inquirer.prompt([
+        
+            {
+            name: "amount",
+            message: "Enter you amount",
+            type: "number",
+           } 
+]);       
+// = += -=
+myBalance -= amountAns.amount;
+
+console.log("your remaining balance is " + myBalance)
+
+
+    if (operationAns.operation === "check balance") {
+    console.log(`your remaining balance is ${myBalance}`)
+    }
+
+
+else {
+    console.log("Incorrect pin code");
+}
+
+
+
+    
+
+
